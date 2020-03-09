c3e625cbea608df9ff141acdfc05d12fa6131a92
Ryan T. Hamilton <astrobokonon@gmail.com>
2020-03-08 21:51:54 -0700

    Clean up and make it reboot if/when if fails

--- boot.py
+++ boot.py
@@ -1,22 +1,18 @@
-# This file is executed on every boot (including wake-boot from deepsleep)
 import gc
 import esp
 esp.osdebug(0)
 
 # Adding in a few here even if they're not called directly to make sure
 #   they're always available in an interactive session
-import os
 import time
-import network
-import webrepl
+import machine
 import ubinascii
 import ujson as json
-from machine import Pin, PWM, Timer
+from machine import Pin
 
 import utils
 import utils_wifi as uwifi
 import wemoButton as wemobutt
-
 
 # Init the stuff
 # WiFi Status, cmd status, onboard
@@ -31,7 +27,7 @@ ledStatus.on()
 ledBuiltin.on()
 
 # This gives time for any automatic wifi connection to finish
-time.sleep(3)
+time.sleep(5)
 
 # Define the known access points to try to connect to, and make them global
 #   so the main loop can access them when/if needed
@@ -50,11 +46,9 @@ if conncheck is True:
 else:
     utils.blinken(ledStatus, 0.25, 10)
     ledStatus.on()
-
-# In case you want the MAC address, here it is in two parts
-# macaddr = ubinascii.hexlify(wlan.config('mac'),':').decode()
-# mac1 = macaddr[0:8]
-# mac2 = macaddr[8:]
+# In case you want the MAC address, here it is
+macaddr = ubinascii.hexlify(wlan.config('mac'),':').decode()
+print("Device MAC:", macaddr)
 
 # Ok, give even a little more time for things to settle before
 #   we move on to main.py
@@ -64,9 +58,20 @@ time.sleep(2)
 ledBuiltin.off()
 gc.collect()
 
+# At this point, you're ready to go.  Define your specific sensor needs,
+#   then import your main loop and call it
+
 # Start the main loop
-wemobutt.looper(knownaps, wlan, conncheck, wconfig,
-                trigger=butt,
-                ledTrigger=ledTrigger,
-                ledStatus=ledStatus,
-                ledBuiltin=ledBuiltin)
+try:
+    wemobutt.looper(knownaps, wlan, conncheck, wconfig,
+                    trigger=butt,
+                    ledTrigger=ledTrigger,
+                    ledStatus=ledStatus,
+                    ledBuiltin=ledBuiltin)
+except:
+    print("Major fail :(")
+
+# Drop the hammer and just reset this goddamn thing
+print("Resetting in 5 seconds ...")
+time.sleep(5)
+machine.reset()
